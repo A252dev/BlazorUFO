d6eac6b97883874d36a1d98d72f2ccdd52fa568f
a252dev <a252p@outlook.com>
2024-04-09 19:04:28 +0200

    0.1

---
+++ wwwroot/js/profile.js
@@ -0,0 +1,23 @@
+const user_info = document.getElementById("user_info");
+const user_balance = document.getElementById("user_balance");
+
+const user_balance_button = document.getElementById("user_balance_button")
+const user_info_button = document.getElementById("user_info_button");
+
+console.log("Handler is loaded");
+
+function initQueryElements(){
+    console.log("started...");
+}
+
+// document.getElementById(user_balance_button).onclick(function (){
+//     console.log("hidden");
+//     document.getElementById(user_balance).className.replace("hidden", "");
+//     document.getElementById(user_info).className.replace("", "hidden");
+// });
+
+// jQuery(user_balance_button).onclick(function (){
+//     console.log("hidden");
+//     $(user_balance).className.remove("hidden");
+//     $(user_info).className.add("hidden");
+// });
